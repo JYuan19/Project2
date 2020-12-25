8684022c7975dd03dafc25a292b0fa3b103dcfab
JunYuan <66101923+JYuan19@users.noreply.github.com>
2020-12-25 11:17:29 +0800

    Update deleteCategory.php

--- Superadmin/food/deleteCategory.php
+++ Superadmin/food/deleteCategory.php
@@ -3,11 +3,10 @@
 session_start();
 try {
 
-    if (!file_exists('../../database/pdo.php' ))
-        throw new Exception();
-    else
-        require_once('../../database/pdo.php' ); 
-		
+	if (!file_exists('../../database/pdo.php'))
+		throw new Exception();
+	else
+		require_once('../../database/pdo.php');
 } catch (Exception $e) {
 
 	$_SESSION['msg'] = 'There were some problem in the Server! Try after some time!';
@@ -15,7 +14,6 @@ try {
 	header('location: category.php');
 
 	exit();
-	
 }
 
 if (!isset($_REQUEST['c_id'])) {
@@ -25,24 +23,22 @@ if (!isset($_REQUEST['c_id'])) {
 	header('location: category.php');
 
 	exit();
-} 
+}
 
-	$c_id = $_REQUEST['c_id'];
+$c_id = $_REQUEST['c_id'];
 
 
-	$sql = "DELETE category , food FROM category INNER JOIN food WHERE category.c_id= food.cart_id and category.c_id = ? ";
-    $query  = $pdoconn->prepare($sql);
-    if ($query->execute([$c_id])) {
+$sql = "DELETE FROM category  WHERE c_id = ? ";
+$query  = $pdoconn->prepare($sql);
+$sqldelete = "DELETE FROM food  WHERE cart_id =$c_id ";
+$querydelete  = $pdoconn->prepare($sqldelete);
+if ($query->execute([$c_id])) {
+	if ($querydelete->execute([$c_id])) {
+	}
+	header('location: category.php');
+} else {
 
-    	$_SESSION['msg'] = 'Category Deleted!';
+	$_SESSION['msg'] = 'There were some problem in the server! Please try again after some time!';
 
-		header('location: category.php');
-    	
-    } else {
-
-    	$_SESSION['msg'] = 'There were some problem in the server! Please try again after some time!';
-
-		header('location: category.php');
-
-    }
-
+	header('location: category.php');
+}
