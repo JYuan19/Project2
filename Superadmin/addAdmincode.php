46c758beb56b875394830e1bbf3d7843b075d12e
JunYuan <66101923+JYuan19@users.noreply.github.com>
2020-12-25 11:18:49 +0800

    Update addAdmincode.php

--- Superadmin/addAdmincode.php
+++ Superadmin/addAdmincode.php
@@ -10,17 +10,9 @@ if(isset($_POST['insert'])) {
      $Position = $_POST['Position'];
 
    $sql = "INSERT into admin(ad_id, Name, Email, Password,Position)values('', '$Name', '$Email', '$Password', '$Position')";
- $query = mysqli_query($conn,$sql );
+  $query = mysqli_query($conn,$sql );
    echo "<script>alert('Congratulations,Insert Succesful!!!'); window.location.assign('adminPage.php');</script>";
        
-   } else {
-    $query="UPDATE requestjob 
-    SET status = 'have not approve' 
-    WHERE id = '".$_SESSION["id"]."'";
-  $statement = $connect->prepare($query);
-  
-  $statement->execute();
-       echo "<script>alert('Invalid !'); window.location.assign('adminPage.php');</script>";
-   }
+   } 
 
 ?>
