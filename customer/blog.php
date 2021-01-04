17d23dc503b725e6694947ec67054fb8263a97a6
JunYuan <66101923+JYuan19@users.noreply.github.com>
2021-01-04 19:37:39 +0800

    Update blog.php

--- customer/blog.php
+++ customer/blog.php
@@ -10,7 +10,7 @@ $conn = mysqli_connect($servername, $username, $password,$db);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$limit = 4;
+$limit = 3;
 $sql = "SELECT COUNT(id) FROM blog";  
 $rs_result = mysqli_query($conn, $sql);  
 $row = mysqli_fetch_row($rs_result);  
